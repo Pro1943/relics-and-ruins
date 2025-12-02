17fa71e2a36a3dc21f2515decd3568a13ccd5dd2
Pieater9000 <pythonenabled@gmail.com>
2025-12-02 18:26:13 +0530

    Initial

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from clear_terminal import clear_terminal
+from ruinlist import ruin_list
+
 def main():
-    print("Hello World!")
+    print("===Welcome To Relics And Runes===")
+    print("Chose your Ruin: ")
+    ruin_list()
+    input()
 
 if __name__ == "__main__":
     main()
---
+++ ruinlist.py
@@ -0,0 +1,2 @@
+def ruin_list():
+    print("1. Fire \n2. Water\n3. Earth\n4. Light")
