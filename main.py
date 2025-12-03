e0b6c9405d083d8d73124b3c30fcbf9de7b9bb0a
Pieater9000 <pythonenabled@gmail.com>
2025-12-03 19:43:32 +0530

    Initial

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     #Start screen
     print("Centuries ago, a powerful civilization fell after misusing ancient relics.")
     print("Now, the Ruin Warden — a corrupted guardian — controls the heart of the old kingdom.")
-    print("You’re a scavenger trying to uncover relics, survive the ruins, and eventually confront the Warden before the corruption spreads.")
+    print("You're a scavenger trying to uncover relics, survive the ruins, and eventually confront the Warden before the corruption spreads.")
 
     #Intro
     while True:
@@ -24,15 +24,28 @@ def main():
             if introchoice2 == 1:
                 ct()
                 print("You decided to knock on the door...")
+                break
             else:
                 print("You decided to go back")
                 ct()
+                continue
         else:
             ct()
             print("You decided to go the the forest.")
             forest_start()
             forestchoice = int(input(">"))
-
+            if forestchoice == 1 and village_key == True:
+                ct()
+                print("You entered the forest!")
+                break
+            elif forestchoice == 1 and village_key == False:
+               ct()
+               print("There seems to be a miserious barrier that is stoping you from entering!")
+               break
+            elif forestchoice == 2:
+                print("You decided to go back.")
+                ct()
+                continue
 
 
 
