dd464c8b2a39f1187af11253fbd5014c615657d4
Pieater9000 <pythonenabled@gmail.com>
2025-12-04 15:57:21 +0530

    Items_Modified

---
+++ item_states.py
@@ -0,0 +1,10 @@
+from loot_table import bone, leather, iron_scrap, gold_scrap
+
+def bone_states():
+    return("Bone:\n",bone())
+def leather_states():
+    return("Leather:\n",leather())
+def iron_scrap_states():
+    return("Iron Scrap:\n",iron_scrap())
+def gold_scrap_states():
+    return("Gold Scrap:\n",gold_scrap())
--- loot_table.py
+++ loot_table.py
@@ -2,14 +2,36 @@
 def bone():
     loot_type = "common"
     saleable = True
+    function = "Selling only"
+    return {
+        "loot_type": "common",
+        "saleable": True,
+        "function": "Selling only"
+    }
 def leather():
     loot_type = "common"
     saleable = True
     function = "armor_player"
+    return {
+        "loot_type": "common",
+        "saleable": True,
+        "function": "armor_player"
+    }
 def iron_scrap():
     loot_type = "common"
     function = "armor_player"
     saleable = True
+    return {
+        "loot_type": "common",
+        "saleable": True,
+        "function": "armor_player"
+    }
 def gold_scrap():
     loot_type = "rare"
     function = "armor_player"
+    saleable = True
+    return {
+        "loot_type": "rare",
+        "saleable": True,
+        "function": "armor_player"
+    }
