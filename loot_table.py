d667c8b863007cf09e9ff442faa245700007b787
najesh <najeshafrozshah-create>
2025-12-05 21:34:44 +0530

    add more loots

--- loot_table.py
+++ loot_table.py
@@ -1,12 +1,17 @@
 #Loot_table of the Player
+# Mainly, there are 3 types of loots: 
+# 1. Common
+# 2. rare
+# 3. epic 
+            
 def bone():
     loot_type = "common"
     saleable = True
-    function = "Selling only"
+    function = "selling only"
     return {
         "loot_type": "common",
         "saleable": True,
-        "function": "Selling only"
+        "function": "selling only"
     }
 def leather():
     loot_type = "common"
@@ -34,4 +39,31 @@ def gold_scrap():
         "loot_type": "rare",
         "saleable": True,
         "function": "armor_player"
+    }
+def dragon_eye():
+    loot_type = "epic"
+    function = "selling only"
+    saleable = True
+    return {
+        "loot_type": "epic",
+        "saleable" : True,
+        "function" : "selling only"
+    }
+def diamond():
+    loot_type = "epic"
+    function = "selling only"
+    saleable = True
+    return {
+        "loot_type": "epic",
+        "saleable" : True,
+        "function" : "selling only"
+    }
+def ruby():
+    loot_type = "rare"
+    function = "selling only"
+    saleable = True
+    return {
+        "loot_type": "rare",
+        "saleable" : True,
+        "function" : "selling only"
     }
