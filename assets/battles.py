f0c40a3037c50ca836d7ea169b7b0d5a21b45c04
Najesh Afroz Shah <najeshafrozshah@gmail.com>
2025-12-19 19:39:13 +0530

    GUI

--- assets/battles.py
+++ assets/battles.py
@@ -4,4 +4,4 @@ from .enemy import GHOST_ART
 def intro_fight_state(state):
     """Prepare the state for an intro fight and return an initial message."""
     state.reset_combat()
-    return {"msg": "A wild ghost appears!", "state": {"player_hp": state.player_hp, "enemy_hp": state.enemy_hp}} and {GHOST_ART}
+    return {"msg": "A wild ghost appears!", "state": {"player_hp": state.player_hp, "enemy_hp": state.enemy_hp}}
