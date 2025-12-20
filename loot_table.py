7ca4567a7f9761f2fc805b1a50d03cb7f13ca327
Najesh Afroz Shah <najeshafrozshah@gmail.com>
2025-12-20 12:52:11 +0530

    Update Loot Table Logic

--- loot_table.py
+++ loot_table.py
@@ -6,63 +6,42 @@ loots : bone, leather, iron scrap, gold scrap, dragon eye, diamond, ruby, berry
   '''
             
 def bone():
-    loot_type = "common"
-    saleable = True
-    function = "selling only"
     return {
         "loot_type": "common",
         "saleable": True,
         "function": "selling only"
     }
 def leather():
-    loot_type = "common"
-    saleable = True
-    function = "armor_player"
     return {
         "loot_type": "common",
         "saleable": True,
         "function": "armor_player"
     }
 def iron_scrap():
-    loot_type = "common"
-    function = "armor_player"
-    saleable = True
     return {
         "loot_type": "common",
         "saleable": True,
         "function": "armor_player"
     }
 def gold_scrap():
-    loot_type = "rare"
-    function = "armor_player"
-    saleable = True
     return {
         "loot_type": "rare",
         "saleable": True,
         "function": "armor_player"
     }
 def dragon_eye():
-    loot_type = "epic"
-    function = "selling only"
-    saleable = True
     return {
         "loot_type": "epic",
         "saleable" : True,
         "function" : "selling only"
     }
 def diamond():
-    loot_type = "epic"
-    function = "selling only"
-    saleable = True
     return {
         "loot_type": "epic",
         "saleable" : True,
         "function" : "selling only"
     }
 def ruby():
-    loot_type = "rare"
-    function = "selling only"
-    saleable = True
     return {
         "loot_type": "rare",
         "saleable" : True,
